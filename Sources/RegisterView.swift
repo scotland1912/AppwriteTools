2916fabb4f4a68a3122dc73906ca7c32ad2e4db9
scotland1912 <122184782+scotland1912@users.noreply.github.com>
2024-06-24 14:42:34 +0200

    [Feature] Add RegisterView

--- Sources/RegisterView.swift
+++ Sources/RegisterView.swift
@@ -2,29 +2,28 @@ import Foundation
 import SwiftUI
 import Appwrite
 
-public struct AppwriteLoginView: View {
+public struct AppwriteRegisterView: View {
     
     @Binding var email: String
     @Binding var password: String
+    @Binding var confirmPassword: String
     @Binding var isLoading: Bool
-    @State private var showResetPasswordOption: Bool
-    @State private var showRegisterOption: Bool
-    @State var loginAction: (() -> Void)
+    @State private var showLoginOption: Bool
     @State var registerAction: (() -> Void)
-    @State var resetPasswordAction: (() -> Void)
+    @State var loginAction: (() -> Void)
     @State var invertedLightMode: Bool
     
-    public init(email: Binding<String>, password: Binding<String>, isLoading: Binding<Bool>, resetPasswordAction: (() -> Void)?, loginAction: @escaping (() -> Void), registerAction: (() -> Void)?, invertedLightMode: Bool) {
+    public init(email: Binding<String>, password: Binding<String>, confirmPassword: Binding<String>, isLoading: Binding<Bool>, registerAction: @escaping () -> Void, loginAction: (() -> Void)?, invertedLightMode: Bool) {
         self._email = email
         self._password = password
+        self._confirmPassword = confirmPassword
         self._isLoading = isLoading
-        self.showResetPasswordOption = resetPasswordAction != nil
-        self.showRegisterOption = registerAction != nil
-        self.loginAction = loginAction
-        self.registerAction = registerAction ?? {}
-        self.resetPasswordAction = resetPasswordAction ?? {}
+        self.showLoginOption = loginAction != nil
+        self.registerAction = registerAction
+        self.loginAction = loginAction ?? {}
         self.invertedLightMode = invertedLightMode
     }
+    
     
     public var body: some View {
         VStack {
@@ -36,18 +35,20 @@ public struct AppwriteLoginView: View {
                         .textContentType(.emailAddress)
                         .keyboardType(.emailAddress)
                     SecureField("Passwort", text: $password)
-                        .textContentType(.password)
+                        .textContentType(.newPassword)
+                    SecureField("Passwort bestätigen", text: $confirmPassword)
+                        .textContentType(.newPassword)
                 }
                 .listRowBackground(invertedLightMode ? Color(uiColor: .secondarySystemBackground) : Color(uiColor: .secondarySystemGroupedBackground))
                 Section {
-                    Button(action: loginAction) {
+                    Button(action: registerAction) {
                         VStack {
                             if isLoading {
                                 ProgressView()
                                     .progressViewStyle(.circular)
                                     .tint(.blue)
                             } else {
-                                Text("Anmelden")
+                                Text("Account erstellen")
                                     .bold()
                             }
                         }
@@ -58,31 +59,18 @@ public struct AppwriteLoginView: View {
                     .buttonStyle(.borderedProminent)
                     .frame(height: 50)
                     .opacity(isLoading ? 0.5 : 1.0)
-                    .disabled(password.count < 8 || !email.isValidEmail)
+                    .disabled(password.count < 8 || password != confirmPassword || !email.isValidEmail)
                     .listRowInsets(EdgeInsets())
-                    if showResetPasswordOption {
-                        HStack {
-                            Spacer(minLength: 0)
-                            VStack {
-                                Text("Passwort vergessen?")
-                                Button("Zurücksetzen", action: resetPasswordAction)
-                                    .buttonStyle(.borderless)
-                            }
-                            Spacer(minLength: 0)
-                        }
-                        .font(.footnote)
-                        .listRowBackground(invertedLightMode ? Color(uiColor: .systemBackground) : Color(uiColor: .systemGroupedBackground))
-                    }
                 }
                 .background(invertedLightMode ? Color(uiColor: .systemBackground) : Color(uiColor: .systemGroupedBackground))
             }
             .scrollContentBackground(invertedLightMode ? .hidden : .visible)
             .listSectionSpacing(32)
             .listRowSpacing(16)
-            if showRegisterOption {
-                Text("Du hast noch keinen Account?")
+            if showLoginOption {
+                Text("Du hast bereits einen Account?")
                     .font(.footnote)
-                Button("Account erstellen", action: registerAction)
+                Button("Anmelden", action: loginAction)
                     .font(.footnote)
             }
         }
@@ -91,5 +79,5 @@ public struct AppwriteLoginView: View {
 }
 
 #Preview {
-    AppwriteLoginView(email: .constant("test@test.test"), password: .constant("testtest"), isLoading: .constant(false), resetPasswordAction: { print("Reset Password") }, loginAction: { print("Login") }, registerAction: { print("Register") }, invertedLightMode: false)
+    AppwriteRegisterView(email: .constant("test@test.test"), password: .constant("testtest"), confirmPassword: .constant("testtest"), isLoading: .constant(false), registerAction: { print("Register") }, loginAction: { print("Login") }, invertedLightMode: true)
 }
